e0de9ebccf99590b5ebf5354b8f0124f84076ca3
Emirhan Gocturk <emirg@protonmail.ch>
2019-09-17 11:12:17 +0200

    64 prescale with 31250 TOP

--- Timers/Timers/main.c
+++ Timers/Timers/main.c
@@ -13,12 +13,13 @@ int extraTimer = 0;
 
 int main(void)
 {
-	DDRB = 0x01;
-	TCCR1B = (1 << CS12) | (1 << CS10) | (1 << WGM12); //Setting prescaler 1024
-	OCR1A = 195;
-	TIMSK1 = (1 << OCIE1A);
+	DDRB = 0x01; // Set output pin
+	TCCR1B = (1 << CS11) | (1 << CS10); // Setting prescaler 1024
+	TCCR1B |= (1 << WGM12); // Enable PWM signals
+	OCR1A = 31250; // Total timer ticks to reach TOP
+	TIMSK1 = (1 << OCIE1A); //Enable it in timer mask
 	
-	sei();
+	sei(); // Enable global interrupts
 	
     while (1)
     {
@@ -29,9 +30,9 @@ int main(void)
 ISR(TIMER1_COMPA_vect)
 {
 	extraTimer++;
-	if(extraTimer >= 25)
+	if(extraTimer >= 25) // 25 x 0.01s = 0.25s
 	{
 		PORTB ^= (1 << PORTB0);
-		extraTimer = 0;
+		extraTimer = 0; // Reset timer
 	}
 }
