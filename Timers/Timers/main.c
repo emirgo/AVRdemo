569b23d459b1acbc6b7a36d15a798d2ed3cf7c87
Emirhan Gocturk <emirg@protonmail.ch>
2019-09-18 10:41:34 +0200

    Timer Interrupt Precision

--- Timers/Timers/main.c
+++ Timers/Timers/main.c
@@ -14,10 +14,12 @@ int extraTimer = 0;
 int main(void)
 {
 	DDRB = 0x01; // Set output pin
-	TCCR1B = (1 << CS11) | (1 << CS10); // Setting prescaler 1024
-	TCCR1B |= (1 << WGM12); // Enable PWM signals
-	OCR1A = 31250; // Total timer ticks to reach TOP
-	TIMSK1 = (1 << OCIE1A); //Enable it in timer mask
+	
+	TCCR1B = (1 << CS11) | (1 << CS10); // Set prescaler to 64
+	TCCR1B |= (1 << WGM12); // Set CTC
+	
+	OCR1A = 2500; // Comparison TOP value
+	TIMSK1 = (1 << OCIE1A); // Set OCRA1A to interrupt
 	
 	sei(); // Enable global interrupts
 	
