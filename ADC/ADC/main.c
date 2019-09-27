cb9fa3d82d825f1752bcc515cd8af0970735783b
Emirhan Gocturk <emirg@protonmail.ch>
2019-09-27 15:24:57 +0200

    Commentary

--- ADC/ADC/main.c
+++ ADC/ADC/main.c
@@ -13,27 +13,27 @@ int main()
 	DDRB = 0xFF;
 	
 	ADMUX |= (1 << REFS0);// VREF
-	ADMUX |= (1 << MUX0) | (1 << MUX2); //set ADC 5
-	ADMUX |= (1 << ADLAR);
+	ADMUX |= (1 << MUX0) | (1 << MUX2); //set ADC 5 reading analog 5 on arduino uno board
+	ADMUX |= (1 << ADLAR); //left adjust result - adjusts binary to left ADCH and ADCL
 	
 	ADCSRA |= (1 << ADEN); //Enable ADC
 	ADCSRA |= (1 << ADIE); // Enable Interrupts for ADC
 	
 	ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // Prescaler at 128
-	DIDR0 = (1 << ADC5D);
+	DIDR0 = (1 << ADC5D); // Disable digital input buffer on analog pin 5
 	ADCSRA |= (1 << ADSC); //Start conversion
 	
 	sei(); // interrupts
 	
 	while(1)
 	{
-		
+		// xxxx xx11 1010 1010
 	}
 }
 
 //Start the next conversion - ISR
 ISR(ADC_vect)
 {
-	PORTB = ADCH >> 3;
+	PORTB = ADCH >> 3; //MSB of ADCH is set to PORTB as a whole
 	ADCSRA |= (1 << ADSC); //Start conversion
 }
