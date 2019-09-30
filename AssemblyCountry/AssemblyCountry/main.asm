e5072a04700e2ffe843b6e37fd465d67699a6773
Emirhan Gocturk <emirg@protonmail.ch>
2019-09-30 22:20:31 +0200

    Update main.asm

--- AssemblyCountry/AssemblyCountry/main.asm
+++ AssemblyCountry/AssemblyCountry/main.asm
@@ -8,20 +8,20 @@
 .DEF tmp = r16 // define register 16 as tmp
 .DEF count = r17 // define register 17 as count
 
-MAIN:
+main:
     clr tmp	// clear tmp -- set all bits zero
 	ldi tmp, 10 // load 10 into tmp
     out DDRD, tmp // output tmp to DDRD
 	ldi count, 10 // load 10 into count
 
-LOOP1:
+loop1:
 	inc tmp // increment tmp by one until 20
 	out DDRD, tmp // output tmp value to DDRD
 	dec count // decrement count by one
 	brne loop1 // if count isn't zero continue loop
 
 ldi count, 10 // reset/load count to 10
-LOOP2:
+loop2:
 	dec tmp	// decrement tmp which was 20
 	out DDRD, tmp // output tmp value to DDRD
 	dec count // decrement count
